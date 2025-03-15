0735afab46dd67b1bf73e92d3df495628d26c327
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-15 21:33:46 +0330

    FEAT(details):writing INTERNAL_IPS(debug) in the file setting

--- src/settings.py
+++ src/settings.py
@@ -143,3 +143,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#   django debug toolbar
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
