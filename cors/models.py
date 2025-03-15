65d6acca548cfd61b7d5fecf75aeda10a0711c1c
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-15 22:04:32 +0330

    FEAT(details):writing file in model user in app cors

--- cors/models.py
+++ cors/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from rest_framework_simplejwt.tokens import RefreshToken
+from .managers import UserManager
+
+
 
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True,verbose_name="Email")
+    # username = models.CharField(max_length=50, unique=True, verbose_name="Username")
+    # phone_regex = RegexValidator(regex=r'^(0|0098|\+98)?9(0[1-5]|[1-3]\d|2[0-2]|9[0-9])\d{7}$',
+    #                              message='The entered phone number format is incorrect.')
+    # phone = models.CharField(validators=[phone_regex], max_length=11, blank=True, verbose_name="Phone number")
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
+
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
