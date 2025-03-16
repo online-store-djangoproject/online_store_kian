0df7f19b72c64fd91ba14f8acf8a985b8d3bda10
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-16 19:20:23 +0330

    FEAT(details):writing login-method-1 in view.py

--- cors/views.py
+++ cors/views.py
@@ -28,14 +28,14 @@ class UserRegisterView(GenericAPIView):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# # login method 1
-#
-# class UserLoginView(APIView):
-#     def post(self, request):
-#         serializer = UserLoginSerializer(data=request.data)
-#         if serializer.is_valid(raise_exception=True):
-#             return Response({
-#                 "data": serializer.validated_data,
-#                 "message": "Login successful."
-#             }, status=status.HTTP_200_OK)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# login method 1
+
+class UserLoginView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            return Response({
+                "data": serializer.validated_data,
+                "message": "Login successful."
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
