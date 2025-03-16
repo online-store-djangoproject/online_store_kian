963e6e3de289823279275ca0d7288b3089e10b26
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-16 21:04:27 +0330

    FEAT(details):writing password in serializers.py

--- cors/serializers.py
+++ cors/serializers.py
@@ -126,62 +126,62 @@ class UserOTPVerifySerializer(serializers.Serializer):
 
 
 
-# class PasswordResetRequestSerializer(serializers.Serializer):
-#     email = serializers.EmailField()
-#
-#     class Meta:
-#         fields = ['email']
-#
-#     def validate(self, attrs):
-#         email = attrs.get('email')
-#         if User.objects.filter(email=email).exists():
-#             user = User.objects.get(email=email)
-#             uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
-#             token = PasswordResetTokenGenerator().make_token(user)
-#             request = self.context.get('request')
-#             site_domain = get_current_site(request).domain
-#             relative_link = reverse('passcon')  # به جای مسیر API از مسیر فرانت استفاده کنید
-#             abslink = f'http://{site_domain}{relative_link}?uidb64={uidb64}&token={token}'
-#             email_body = f'Hi use the link below to reset your password \n {abslink}'
-#             data = {
-#                 'email_body': email_body,
-#                 'email_subject': 'Reset your password',
-#                 'to_email':user.email,
-#             }
-#             send_password_email(data)
-#
-#         return super().validate(attrs)
-#
-# #                 SetNewPasswordSerializer
-#
-# class SetNewPasswordSerializer(serializers.Serializer):
-#     password = serializers.CharField(write_only=True)
-#     confirm_password = serializers.CharField(write_only=True)
-#     uidb64 = serializers.CharField(write_only=True)
-#     token = serializers.CharField(write_only=True)
-#
-#     class Meta:
-#         fields = ['password', 'confirm_password', 'uidb64', 'token']
-#
-#     def validate(self, attrs):
-#         try:
-#             token = attrs.get('token')
-#             uidb64 = attrs.get('uidb64')
-#             password = attrs.get('password')
-#             confirm_password = attrs.get('confirm_password')
-#
-#             user_id = force_str(urlsafe_base64_decode(uidb64))
-#             user = User.objects.get(pk=user_id)
-#             if not PasswordResetTokenGenerator().check_token(user, token):
-#                 raise AuthenticationFailed('reset link is invalid or has expired.')
-#             if password != confirm_password:
-#                 raise AuthenticationFailed('password and confirm_password do not match.')
-#             user.set_password(password)
-#             user.save()
-#             return user
-#
-#         except Exception as e:
-#             raise AuthenticationFailed('link is invalid or has expired.')
+class PasswordResetRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    class Meta:
+        fields = ['email']
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email=email)
+            uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
+            token = PasswordResetTokenGenerator().make_token(user)
+            request = self.context.get('request')
+            site_domain = get_current_site(request).domain
+            relative_link = reverse('passcon')  # به جای مسیر API از مسیر فرانت استفاده کنید
+            abslink = f'http://{site_domain}{relative_link}?uidb64={uidb64}&token={token}'
+            email_body = f'Hi use the link below to reset your password \n {abslink}'
+            data = {
+                'email_body': email_body,
+                'email_subject': 'Reset your password',
+                'to_email':user.email,
+            }
+            send_password_email(data)
+
+        return super().validate(attrs)
+
+#                 SetNewPasswordSerializer
+
+class SetNewPasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+    uidb64 = serializers.CharField(write_only=True)
+    token = serializers.CharField(write_only=True)
+
+    class Meta:
+        fields = ['password', 'confirm_password', 'uidb64', 'token']
+
+    def validate(self, attrs):
+        try:
+            token = attrs.get('token')
+            uidb64 = attrs.get('uidb64')
+            password = attrs.get('password')
+            confirm_password = attrs.get('confirm_password')
+
+            user_id = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=user_id)
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                raise AuthenticationFailed('reset link is invalid or has expired.')
+            if password != confirm_password:
+                raise AuthenticationFailed('password and confirm_password do not match.')
+            user.set_password(password)
+            user.save()
+            return user
+
+        except Exception as e:
+            raise AuthenticationFailed('link is invalid or has expired.')
 
 #
 # class LogoutUserSerializer(serializers.Serializer):
