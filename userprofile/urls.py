273b6faaeda7380107c54beac613447db4f68454
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-18 19:00:33 +0330

    FEAT(details):creating and start address in urls.py

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('addresses/', views.AddressListCreateView.as_view(), name="address-list"),
+    path('addresses/<int:pk>/', views.AddressUpdateDeleteView.as_view(), name="address-detail"),
+
 
 ]
