0410d73f1c6816cbdd59845e001e6a556208c863
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-18 18:57:54 +0330

    FEAT(details):creating and start customer in serializers.py

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,15 @@
+from django.core.validators import RegexValidator
 from django.db import models
+from cors.models import User
+
 
 # Create your models here.
+
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.email
