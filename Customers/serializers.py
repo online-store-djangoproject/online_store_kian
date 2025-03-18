a06480e2666063a2c53a67df18ef2853eb3c3874
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-18 18:50:48 +0330

    FEAT(details):creating and start customer in serializers.py

---
+++ Customers/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Customer, Address
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['user']
+
+# class AddressSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Address
+#         fields = '__all__'
