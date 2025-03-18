d00118fb917bb20cc6b7bde433e0752837259a4d
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-18 18:39:35 +0330

    FIX(details):changes template profile.html

--- cors/serializers.py
+++ cors/serializers.py
@@ -208,10 +208,11 @@ class LogoutUserSerializer(serializers.Serializer):
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name']
+        fields = ['email','first_name', 'last_name','phone']
 
     def update(self, instance, validated_data):
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.phone = validated_data.get('phone', instance.phone)
         instance.save()
         return instance
