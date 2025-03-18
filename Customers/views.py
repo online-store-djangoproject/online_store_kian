00b508d55212990f240a33c0ae284e4ea2935e56
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-18 18:52:31 +0330

    FEAT(details):creating and start address in view.py

--- Customers/views.py
+++ Customers/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status, generics
+from .models import Customer, Address
+from .serializers import CustomerSerializer, AddressSerializer
 # Create your views here.
+
+
+class AddressListCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = AddressSerializer
+
+    def get_queryset(self):
+        return Address.objects.filter(customer=self.request.user.customer)
+
+    def perform_create(self, serializer):
+        serializer.save(customer=self.request.user.customer)
+
+
+class AddressUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = AddressSerializer
+    queryset = Address.objects.all()
+
+    def get_queryset(self):
+        return Address.objects.filter(customer=self.request.user.customer)
