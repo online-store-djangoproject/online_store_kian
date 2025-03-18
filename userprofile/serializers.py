6bece24f0dbc1c38df7f64f2a0bef9b212ae3213
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-18 18:59:19 +0330

    FEAT(details):creating and start address in serializers.py

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -5,3 +5,9 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['user']
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
