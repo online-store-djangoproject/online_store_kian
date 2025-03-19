effa9992859ac680d6fe59047243358bdb7585a9
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-20 01:00:55 +0330

    FEAT(details):creating and start Product & Category admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
+# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ['id','name','discount','price','category']
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Product, ProductAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ['category_id','title']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+
 
 # Register your models here.
--- products/models.py
+++ products/models.py
@@ -3,8 +3,8 @@ import uuid
 # Create your models here.
 
 class Category(models.Model):
-    title = models.CharField(max_length=200)
     category_id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, unique=True)
+    title = models.CharField(max_length=200)
     slug = models.SlugField(default= None)
     featured_product = models.OneToOneField('Product', on_delete=models.CASCADE, blank=True, null=True, related_name='featured_product')
     icon = models.CharField(max_length=100, default=None, blank = True, null=True)
@@ -24,14 +24,14 @@ class Review(models.Model):
 
 
 class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, unique=True)
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     discount = models.BooleanField(default=False)
-    image = models.ImageField(upload_to='img', blank=True, null=True, default='')
+    image = models.ImageField(upload_to='img/product/', blank=True, null=True, default='default.jpg')
     price = models.FloatField(default=100.00)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name='products')
     slug = models.SlugField(default=None)
-    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, unique=True)
     inventory = models.IntegerField(default=5)
     top_deal = models.BooleanField(default=False)
     flash_sales = models.BooleanField(default=False)
