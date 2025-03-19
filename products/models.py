9bd16b2bb85c1d5a860e1a3c8489ac8a2c749d6c
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-20 00:47:53 +0330

    FEAT(details):creating and start Category(model) models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    category_id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, unique=True)
+    slug = models.SlugField(default= None)
+    featured_product = models.OneToOneField('Product', on_delete=models.CASCADE, blank=True, null=True, related_name='featured_product')
+    icon = models.CharField(max_length=100, default=None, blank = True, null=True)
+
+    def __str__(self):
+        return self.title
