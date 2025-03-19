25e810760e5f9ea256e46289e808daf98fdf2ffd
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-20 00:49:13 +0330

    FEAT(details):creating and start Product(model) models.py

--- products/models.py
+++ products/models.py
@@ -21,3 +21,20 @@ class Review(models.Model):
 
     def __str__(self):
         return self.description
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    discount = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='img', blank=True, null=True, default='')
+    price = models.FloatField(default=100.00)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name='products')
+    slug = models.SlugField(default=None)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, unique=True)
+    inventory = models.IntegerField(default=5)
+    top_deal = models.BooleanField(default=False)
+    flash_sales = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
