b7e89f510599ef6da470c1c56df2068232210065
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-19 16:32:18 +0330

    FEAT(details):creating and start user profile in admin.py

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Address,Customer
 # Register your models here.
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    model = Customer
+    list_display = ['user','created_at']
+
+
+admin.site.register(Customer,CustomerAdmin)
+
+
+
+class AddressAdmin(admin.ModelAdmin):
+    model = Address
+    list_display = ['id', 'customer','city','state','full_address','postal_code']
+
+admin.site.register(Address, AddressAdmin)
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addresses/', views.AddressListCreateView.as_view(), name="address-list"),
-    path('addresses/<int:pk>/', views.AddressUpdateDeleteView.as_view(), name="address-detail"),
+    path('addresses/', views.AddressListCreateView.as_view(), name="address_list"),
+    path('addresses/<int:pk>/', views.AddressUpdateDeleteView.as_view(), name="address_detail"),
 
 
 ]
