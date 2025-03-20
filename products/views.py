da79d120f35f736f647a80b2700abd7b55e146b1
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-20 03:41:08 +0330

    FEAT(details):creating and start Category Serializer.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .serializers import *
+from products.models import *
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
+
+
