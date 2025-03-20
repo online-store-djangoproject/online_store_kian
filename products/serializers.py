e159ee04585bf62f640963684abd79f4fa54f301
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-20 03:41:52 +0330

    FEAT(details):creating and start Product Serializer.py

--- products/serializers.py
+++ products/serializers.py
@@ -7,3 +7,11 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["category_id", "title", "slug"]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "name", "description", "category", "slug", "inventory", "price"]
+
+    category = CategorySerializer()
