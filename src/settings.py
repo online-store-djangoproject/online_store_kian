0d3d452b0b72016a13d0d8303743ef6943557886
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-20 03:53:33 +0330

    FEAT(details):pip install django-filter==24.3

--- src/settings.py
+++ src/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'django_filters',
 
 # App in Project
     'cors.apps.CorsConfig',
