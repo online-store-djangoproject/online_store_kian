4d7af6e6ce5574fccdf966b78c16cf798dd68736
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-20 03:43:23 +0330

    FEAT(details):creating and start Category views.py

--- products/views.py
+++ products/views.py
@@ -13,3 +13,8 @@ from rest_framework import status
 class ProductsViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
