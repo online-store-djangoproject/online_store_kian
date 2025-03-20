aea076a8deee339371fb95fa905bcb4dec907432
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-20 04:07:09 +0330

    FEAT(details):creating and start Review in view.py

--- products/views.py
+++ products/views.py
@@ -27,3 +27,16 @@ class ProductsViewSet(ModelViewSet):
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class ReviewViewSet(ModelViewSet):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs["product_pk"])
+
+    def get_serializer_context(self):
+        return {"product_id": self.kwargs["product_pk"]}
+
+
+
