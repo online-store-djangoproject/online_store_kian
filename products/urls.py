2f56e8a0ac975e09d3dd04daad255f8990647ffc
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-20 03:47:13 +0330

    FEAT(details):pip install drf-nested-routers==0.94.1

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-urlpatterns = [
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 
+
+# router = routers.DefaultRouter()
+
+# router.register("products", views.ProductsViewSet)
+# router.register("categories", views.CategoryViewSet)
+urlpatterns = [
+    # path("", include(router.urls)),
 ]
