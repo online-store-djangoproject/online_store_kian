5a857bb68dd58f837e650e4d983e2ee3609653f0
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-24 21:15:53 +0330

    FEAT(details):writing UpdateOrder OrderViewSet in urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -8,11 +8,11 @@ from rest_framework_nested import routers
 router = routers.DefaultRouter()
 
 router.register("carts", views.CartViewSet)
+router.register("orders", views.OrderViewSet, basename="orders")
 
 
 cart_router = routers.NestedDefaultRouter(router, "carts", lookup="cart")
 cart_router.register("items", views.CartItemViewSet, basename="cart-items")
-
 
 urlpatterns = [
     path("", include(cart_router.urls))
