7302ed16effc18462fc4111caed5504264d69056
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-24 20:11:45 +0330

    FEAT(details):writing model models.py

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+#  CART
+
+class Cart(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
