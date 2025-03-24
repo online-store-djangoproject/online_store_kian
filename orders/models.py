73470715be423c3bcc84c4b1fa48c9e596421fe1
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-24 20:42:53 +0330

    FEAT(details):writing Order(model) in models.py

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 import uuid
 from products.models import Product
-
+from  django.conf import settings
 
 # Create your models here.
 
@@ -19,3 +19,22 @@ class Cartitems(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items", null=True, blank=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True, related_name='cartitems')
     quantity = models.PositiveSmallIntegerField(default=0)
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    pending_status = models.CharField(
+        max_length=50, choices=PAYMENT_STATUS_CHOICES, default='PAYMENT_STATUS_PENDING')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.pending_status
