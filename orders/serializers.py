0248bb3511ee349f2cf46e7b5bc5e49019de9ccf
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-24 21:00:11 +0330

    FEAT(details):writing Order in serializer.py

--- orders/serializers.py
+++ orders/serializers.py
@@ -81,3 +81,11 @@ class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = ["id", "product", "quantity"]
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', "placed_at", "pending_status", "owner", "items"]
