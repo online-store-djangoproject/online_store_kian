4aaaf09a02fcf530ea3f50d0398f7a6e00e0daad
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-24 20:23:19 +0330

    FEAT(details):writing cartitem in serializers.py

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,3 +3,16 @@ from itertools import product
 from django.db import transaction
 from rest_framework import serializers
 from  orders.models import *
+from products.serializers import SimpleProductSerializer
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer(many=False)
+    sub_total = serializers.SerializerMethodField(method_name="total")
+
+    class Meta:
+        model = Cartitems
+        fields = ["id", "cart", "product", "quantity", "sub_total"]
+
+    def total(self, cartitem: Cartitems):
+        return cartitem.quantity * cartitem.product.price
