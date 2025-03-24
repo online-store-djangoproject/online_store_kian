ab955bfa989b345b4b9d0846ad333ee8e30dfd24
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-24 20:12:55 +0330

    FEAT(details):writing model(cartitems) models.py

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import uuid
+from products.models import Product
+
 
 # Create your models here.
 
@@ -11,3 +13,9 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+class Cartitems(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items", null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True, related_name='cartitems')
+    quantity = models.PositiveSmallIntegerField(default=0)
