f27a43678e677e5b0d1fef5c98c07e717ef548d4
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-24 20:34:29 +0330

    FEAT(details):writing Cart in views.py

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from orders.models import Cart, Cartitems
+from orders.serializers import CartSerializer, AddCartItemSerializer, UpdateCartItemSerializer, CartItemSerializer
+
 
 # Create your views here.
+
+
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+
+class CartItemViewSet(ModelViewSet):
+    http_method_names = ["get", "post", "patch", "delete"]
+
+    def get_queryset(self):
+        return Cartitems.objects.filter(cart_id=self.kwargs["cart_pk"])
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return AddCartItemSerializer
+
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+
+        return CartItemSerializer
+
+    def get_serializer_context(self):
+        return {"cart_id": self.kwargs["cart_pk"]}
