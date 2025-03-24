6140b4800db807d587d28d963d7e005f95efbe29
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-24 20:34:45 +0330

    FEAT(details):writing Cart in urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -4,15 +4,15 @@ from . import views
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers
 
-#
-# router = routers.DefaultRouter()
-#
-# router.register("carts", views.CartViewSet)
-#
-#
-# cart_router = routers.NestedDefaultRouter(router, "carts", lookup="cart")
-# cart_router.register("items", views.CartItemViewSet, basename="cart-items")
-#
-# urlpatterns = [
-#     path("", include(cart_router.urls))
-# ]
+
+router = routers.DefaultRouter()
+
+router.register("carts", views.CartViewSet)
+
+
+cart_router = routers.NestedDefaultRouter(router, "carts", lookup="cart")
+cart_router.register("items", views.CartItemViewSet, basename="cart-items")
+
+urlpatterns = [
+    path("", include(cart_router.urls))
+]
