a9a5cc8b68526f2819fc3ff5fc7f458ac0982fb9
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-03-28 18:03:38 +0330

    FEAT(details):writing RequestDiscountCodeAPIView & ValidateDiscountCodeAPIView in urls.py

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('addresses/', views.AddressListCreateView.as_view(), name="address_list"),
     path('addresses/<int:pk>/', views.AddressUpdateDeleteView.as_view(), name="address_detail"),
-    # path("discounts/request/", views.RequestDiscountCodeAPIView.as_view(), name="request_discount_code"),
-    # path("discounts/validate/", views.ValidateDiscountCodeAPIView.as_view(), name="validate_discount_code"),
+    path("discounts/request/", views.RequestDiscountCodeAPIView.as_view(), name="request_discount_code"),
+    path("discounts/validate/", views.ValidateDiscountCodeAPIView.as_view(), name="validate_discount_code"),
 
 
 ]
