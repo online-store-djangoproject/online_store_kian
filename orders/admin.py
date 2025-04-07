7637231a8e6ac796557af03c679626dc3f5fd426
kianjavk <mohammadrezajavaherykian@gmail.com>
2025-04-07 20:05:51 +0330

    FEAT(details):creating and start directory tests and file _init_ & test_models

--- orders/admin.py
+++ orders/admin.py
@@ -15,3 +15,32 @@ class CartitemsAdmin(admin.ModelAdmin):
     list_display = ['id','cart','product','quantity']
 
 admin.site.register(Cartitems, CartitemsAdmin)
+
+
+# Order  OrderItem
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    readonly_fields = ('product', 'quantity')
+    can_delete = False
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'pending_status', 'placed_at')
+    list_filter = ('pending_status', 'placed_at')
+    search_fields = ('id', 'owner__email', 'owner__username')
+    readonly_fields = ('placed_at',)
+    inlines = [OrderItemInline]
+
+    fieldsets = (
+        ('اطلاعات سفارش', {
+            'fields': ('owner', 'pending_status', 'placed_at')
+        }),
+    )
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity')
+    search_fields = ('order__id', 'product__title')
+    list_filter = ('product',)
